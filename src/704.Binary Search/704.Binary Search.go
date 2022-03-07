cc34b93ee10d3ce0592342bdf17b0aa2406695a2
WangYi <73774526+hiWyatt@users.noreply.github.com>
2022-03-07 19:50:30 +0800

    Update 704.Binary Search.go

--- src/704.Binary Search/704.Binary Search.go
+++ src/704.Binary Search/704.Binary Search.go
@@ -1,25 +1,26 @@
 package main
 
-func main() {
-	nums := [...]int{-1, 0, 3, 5, 9, 12}
-	target := 9
-	index := search(nums, target)
-	println(index)
-}
-func search(nums [6]int, target int) int {
-	var right, left, mid int
-	left = 0
-	right = len(nums) - 1
+import "fmt"
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
 	for left <= right {
-		mid = left + (right-left)/2
+		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
-		}
-		if nums[mid] < target {
+		} else if nums[mid] < target {
 			left = mid + 1
 		} else {
 			right = mid - 1
 		}
 	}
 	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 9
+	index := search(nums, target)
+	fmt.Println(index)
 }
